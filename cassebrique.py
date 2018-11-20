6c138c208fc453413a6d97b0713c85929bce5ce0
doucelin <doucelin.pedicone@gmail.com>
2018-11-20 11:12:13 +0100

    raquette et image def

--- cassebrique.py
+++ cassebrique.py
@@ -6,6 +6,8 @@ pygame.init()
 windowH = 800
 windowW = 500
 raquette = pygame.image.load('raquette/raquette.png')
+raquetteW = 204
+raquetteH = 24
 background = (113,177,227)
 window = pygame.display.set_mode((windowH,windowW))
 pygame.display.set_caption("Casse-Brique")
@@ -13,23 +15,24 @@ pygame.display.set_caption("Casse-Brique")
 
 
 def main() :
-	
+	raquetteX = 400
+	raquetteY = 470
 	game_over = False
 	moveLeft = False
 	moveRight = False
 	dx = 0
-	while not game_over : 
+	while not game_over :
 		for event in pygame.event.get() :
-			if event.type == pygame.QUIT : 
+			if event.type == pygame.QUIT :
 				game_over = True
-			#Mouvement raquette 
+			#Mouvement raquette
 			if event.type == pygame.KEYDOWN :
-				if event.key == pygame.K_LEFT : 
+				if event.key == pygame.K_LEFT :
 					moveLeft = True
-					dx += -2
+					dx = -2
 				if event.key == pygame.K_RIGHT :
 					moveRight = True
-					dx += 2
+					dx = 2
 			elif event.type == pygame.KEYUP :
 				if event.key == pygame.K_LEFT :
 					moveLeft = False
@@ -38,8 +41,21 @@ def main() :
 
 
 		window.fill(background)
+		displayRaquette(raquetteX,raquetteY)
 		pygame.display.update()
 
+		if raquetteX - raquetteW/2 == 0:
+			dx = 2
+		if raquetteX + raquetteW/2 == 800:
+			dx = -2
+		raquetteX += dx
+
+def displayRaquette(x,y) :
+	img = raquette
+	displayRect = img.get_rect()
+	displayRect.center = (x,y)
+	window.blit(img,displayRect)
+	pygame.display.update()
 
 main()
 pygame.quit()
