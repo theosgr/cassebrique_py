7ffc39d28e215c9bcc93dd0698ec6da9e68a6bbb
theo grollier <theo.grollier85@gmail.com>
2018-11-20 12:20:50 +0100

    Code

--- cassebrique.py
+++ cassebrique.py
@@ -1,22 +1,35 @@
 import pygame
 import time
+from random import *
 
 pygame.init()
 
 windowH = 800
 windowW = 500
+touchingLowerCase = True
+balle = pygame.image.load('balles/balle1.png')
+balleW = 18
+balleH = 16
 raquette = pygame.image.load('raquette/raquette.png')
 raquetteW = 204
 raquetteH = 24
+estLancee = False
 background = (113,177,227)
 window = pygame.display.set_mode((windowH,windowW))
 pygame.display.set_caption("Casse-Brique")
 
 
 
+
 def main() :
 	raquetteX = 400
 	raquetteY = 470
+	vX = 0
+	vY = 0
+	vitesse_balle = 0
+	angle = 0
+	balleX = 392
+	balleY = 466 - raquetteH
 	game_over = False
 	moveLeft = False
 	moveRight = False
@@ -25,30 +38,40 @@ def main() :
 		for event in pygame.event.get() :
 			if event.type == pygame.QUIT :
 				game_over = True
-			#Mouvement raquette
-			if event.type == pygame.KEYDOWN :
-				if event.key == pygame.K_LEFT :
-					moveLeft = True
-					dx = -2
-				if event.key == pygame.K_RIGHT :
-					moveRight = True
-					dx = 2
-			elif event.type == pygame.KEYUP :
-				if event.key == pygame.K_LEFT :
-					moveLeft = False
-				if event.key == pygame.K_RIGHT :
-					moveRight = False
+			if estLancee() == False:
+				if event.type == pygame.KEYDOWN : 
+					vX = randint(1,7)
+					vY = randint(1,10)
+			if estLancee():
+				if event.type == pygame.KEYDOWN :
+					if event.key == pygame.K_LEFT :
+						moveLeft = True
+						dx = -1
+					if event.key == pygame.K_RIGHT :
+						moveRight = True
+						dx = 1
+				elif event.type == pygame.KEYUP :
+					if event.key == pygame.K_LEFT :
+						moveLeft = False
+					if event.key == pygame.K_RIGHT :
+						moveRight = False
 
 
 		window.fill(background)
+		displayBalle(balleX, balleY)
 		displayRaquette(raquetteX,raquetteY)
+				
 		pygame.display.update()
 
 		if raquetteX - raquetteW/2 == 0:
-			dx = 2
+			dx = 1
 		if raquetteX + raquetteW/2 == 800:
-			dx = -2
+			dx = -1
 		raquetteX += dx
+		balleY += vY
+		balleX += vX
+		if isOver(balleY) :
+			game_over = True
 
 def displayRaquette(x,y) :
 	img = raquette
@@ -56,6 +79,19 @@ def displayRaquette(x,y) :
 	displayRect.center = (x,y)
 	window.blit(img,displayRect)
 	pygame.display.update()
+
+def displayBalle(x,y) :
+	window.blit(balle,(x,y))
+
+def isOver(by) : 
+	touchingLowerCase = by > 500
+	return touchingLowerCase
+
+def estLancee(vvX,vvY) :
+	if vvX != 0 or vvY != 0 :
+		return True
+	else :
+		return False
 
 main()
 pygame.quit()
